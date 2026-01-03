3703bba268408a1b1942d5240cd1d405f0cbe26f
Diptesh Deore <114812520+diptesh17@users.noreply.github.com>
2026-01-03 19:04:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Kth largest element/kth-largest-element.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    // Function to return kth largest element from an array.
+    int KthLargest(vector<int> &arr, int k) {
+        // Your code here
+        
+        priority_queue<int,vector<int>,greater<int>> pq;
+        
+        for(int x : arr)
+        {
+            pq.push(x);
+            
+            if(pq.size() > k)
+            pq.pop();
+        }
+        
+        return pq.top();
+        
+    }
+};
